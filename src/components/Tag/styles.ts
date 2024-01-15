d440fdfeee6da0497f5b27fa45725350d47f0f4b
brunobahri <brunobahri27@gmail.com>
2024-01-14 23:14:18 -0300

    refinando estilizacao

--- src/components/Tag/styles.ts
+++ src/components/Tag/styles.ts
@@ -4,12 +4,10 @@ import { cores } from '../../styles'
 export const TagContainer = styled.div`
   background-color: ${cores.vermelho};
   color: ${cores.bege};
-  display: inline-block;
   padding: 6px 4px;
-  width: 61px;
-  height: 26px;
-  max-width: 100%;
-  text-align: center;
   font-size: 12px;
   font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
